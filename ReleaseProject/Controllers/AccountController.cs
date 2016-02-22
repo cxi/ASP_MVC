2d2891214aca864bb6660727b6c4cc22cd580cfd
cxi <xichao33@hotmail.com>
2016-02-22 11:05:18 +0800

    Login Page
    First version of login page using Asp.net mvc 5, bootstrap, etc

---
+++ ReleaseProject/Controllers/AccountController.cs
@@ -0,0 +1,109 @@
+﻿using ReleaseProject.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace ReleaseProject.Controllers
+{   
+    public class AccountController : Controller
+    {
+
+        private Account userAccount = new Account();
+        //
+        // GET: /User/
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.AccountModel user)
+        {
+            int isAdmin;
+            if (ModelState.IsValid)
+            {
+                if (IsValid(user.UserEmail, user.Password, out isAdmin))
+                {
+                    FormsAuthentication.SetAuthCookie(user.UserEmail, false);
+                    Response.SetCookie(new HttpCookie("UserId", userAccount.UserId.ToString()));
+                    //if log in as administrator user
+                    if (isAdmin == 1)
+                    {
+                        return RedirectToAction("Index", "AdminUser");
+                    }
+                    //if login as normal user
+                    else
+                    {
+                        return RedirectToAction("Index", "Contact");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email/Password incorrect");
+                }
+            }
+            return View();
+        }
+
+
+        //for reset password
+        [HttpGet]
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult ForgotPassword(Models.RestPassModel userEmail)
+        {
+            using (var db = new AccountDBEntities())
+            {
+                if (userEmail != null)
+                {
+                    var user = db.Accounts.FirstOrDefault(u => u.Email == userEmail.UserEmail);
+                    if (user == null)
+                    {
+                        ModelState.AddModelError("error", "Email does not exist");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("error", "New password send to you ,please Check");
+                    }
+                }
+                ModelState.AddModelError("error", "Email does not exist");
+            }
+            
+            return View(userEmail);
+        }
+        
+        //To validate 
+        private bool IsValid(string email, string password, out int isadmin)
+        {
+            var crypto = new SimpleCrypto.PBKDF2();
+
+            isadmin = 0;
+            bool isValid = false;
+            using (var db = new AccountDBEntities())
+            {
+                var user = db.Accounts.FirstOrDefault(u => u.Email == email);
+                if (user != null)
+                {
+                    if (user.Password == password)
+                    {
+                        isValid = true;
+                        isadmin = user.UserType;
+                        userAccount = user;
+                    }
+                }
+            }
+            return isValid;
+        }
+    }
+}
---
+++ ReleaseProject/Controllers/AdminUserController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ReleaseProject.Domain;
+
+namespace ReleaseProject.Controllers
+{
+    public class AdminUserController : Controller
+    {
+        private AccountDBEntities db = new AccountDBEntities();
+
+        //
+        // GET: /AdminUser/
+
+        public ActionResult Index()
+        {
+            return View(db.Accounts.ToList());
+        }
+
+        //
+        // GET: /AdminUser/Details/5
+
+        public ActionResult Details(string id = null)
+        {
+            //Account account = db.Accounts.Find(id);
+            Guid userId = new Guid(id);
+            Account account = db.Accounts.FirstOrDefault(u => u.UserId == userId);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        //
+        // GET: /AdminUser/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /AdminUser/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                account.UserId = Guid.NewGuid();
+                db.Accounts.Add(account);
+                if (!ExistEmail(account))
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError("error", "Email exist");
+            ViewBag.error = TempData["error"];
+            return View(account);
+        }
+
+        //
+        // GET: /AdminUser/Edit/5
+
+        public ActionResult Edit(string id = null)
+        {
+            //Account account = db.Accounts.Find(id);
+            Guid userId = new Guid(id);
+            Account account = db.Accounts.FirstOrDefault(u => u.UserId == userId);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        //
+        // POST: /AdminUser/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                    if (!ExistEmail(account))
+                    {
+                        
+                        db.Entry(account).State = EntityState.Modified;
+                        db.SaveChanges();   
+                         return RedirectToAction("Index");
+                    }
+                }              
+            return View(account);
+        }
+
+        //
+        // GET: /AdminUser/Delete/5
+
+        public ActionResult Delete(string id = null)
+        {
+            //Account account = db.Accounts.Find(id);
+            Guid userId = new Guid(id);
+            Account account = db.Accounts.FirstOrDefault(u => u.UserId == userId);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        //
+        // POST: /AdminUser/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            //Account account = db.Accounts.Find(id);
+            Guid userId = new Guid(id);
+            Account account = db.Accounts.FirstOrDefault(u => u.UserId == userId);
+            if (account != null) { 
+                 db.Accounts.Remove(account);
+                 db.SaveChanges();
+                 return RedirectToAction("Index");
+            }
+            
+            return View(account);
+        }
+
+
+        [HttpGet]
+        public ActionResult ShowContact()
+        {
+            return RedirectToAction("Index", "Contact");
+        }
+
+        private bool ExistEmail(Account acc)
+        {
+            bool existEmail = false;
+            using (var db = new AccountDBEntities())
+            {
+                var user = db.Accounts.FirstOrDefault(u => u.Email == acc.Email);
+                if (user != null && user.UserId != acc.UserId)
+                {
+                    existEmail = true;
+                }
+            }
+            return existEmail;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
